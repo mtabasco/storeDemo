1c7c4cc5475277191e491c33e431dfe1fa0ea3a8
mtabasco <matabasco@gmail.com>
2021-08-02 02:19:59 +0200

    store demo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
 import { Biconomy } from '@biconomy/mexa';
-import { toBuffer } from "ethereumjs-util";
+import { signTypedData_v4 } from 'eth-sig-util';
 import Common, { CustomChain } from '@ethereumjs/common';
 import { Transaction as Tx } from '@ethereumjs/tx';
-import PLATFORM_SPN from './contracts/SapienPlatformSPN.json';
+import PLATFORM_SPN_ABI from './contracts/SapienPlatformSPN.json';
+import BADGE_STORE_ABI from './contracts/BadgeStore.json';
 const abi = require('ethereumjs-abi');
 
 
@@ -13,23 +14,30 @@ const config = {
   POLY_NETWORK_ID: 80001,
   POLY_WS_PROVIDER: 'wss://ws-matic-mumbai.chainstacklabs.com',
   POLY_SPN_TOKEN_ADDRESS: '0x8174Ab11EEd70297311f7318a71d9e9f48466Fff', // Mumbai SPN
+  POLY_BADGE_STORE_ADDRESS: '0xab221c69D8EEcF6aC7944efD4589DA206AE1046C',
   BICONOMY_API_KEY: 'tYSKReKvQ.c2fbc08c-3991-49b8-8ed8-cb945b0e55fe',
+  USER_ADDRESS: '0x39a1e6759982d5409e1a496c31571A9352CA229E',
+  PRIVATE_KEY: 'bda187d9e6482b03ab0d696f5e33de5e63cc4ab11490498cf5d13c6215c4719c',
 };
 
-const privateKey = 'bda187d9e6482b03ab0d696f5e33de5e63cc4ab11490498cf5d13c6215c4719c';
-const publicAddress = '0x39a1e6759982d5409e1a496c31571A9352CA229E';
+// Initialize constants
+const domainType = [
+  { name: "name", type: "string" },
+  { name: "version", type: "string" },
+  { name: "verifyingContract", type: "address" },
+  { name: "salt", type: "bytes32" },
+];
+const metaTransactionType = [
+  { name: "nonce", type: "uint256" },
+  { name: "from", type: "address" },
+  { name: "functionSignature", type: "bytes" }
+];
 
-// utils...
-const constructMetaTransactionMessage = (
-  nonce,
-  chainId,
-  functionSignature,
-  contractAddress
-) => {
-  return abi.soliditySHA3(
-    ['uint256', 'address', 'uint256', 'bytes'],
-    [nonce, contractAddress, chainId, toBuffer(functionSignature)]
-  );
+const domainData = {
+  name: "Sapien Badge Store",
+  version: "v3",
+  verifyingContract: config.POLY_BADGE_STORE_ADDRESS,
+  salt: '0x' + (config.POLY_NETWORK_ID).toString(16).padStart(64, '0')
 };
 
 const getSignatureParameters = (web3, signature) => {
@@ -80,59 +88,57 @@ const getSignatureParameters = (web3, signature) => {
     .onEvent(biconomy.READY, async () => {
       // Initialize your dapp here like getting user accounts etc
       console.log('Mexa is Ready');
-      const contract = new web3.eth.Contract(PLATFORM_SPN as AbiItem[], config.POLY_SPN_TOKEN_ADDRESS);
+      const contract = new web3.eth.Contract(BADGE_STORE_ABI as AbiItem[], config.POLY_BADGE_STORE_ADDRESS);
 
       const functionSignature = contract.methods
-        .transfer('0xeC736346eBf9f995a40006147923CD8Ad7bfb2d7', 1000)
+        .purchaseBadge(7, 1)
         .encodeABI();
 
+      const nonce = await contract.methods.getNonce(config.USER_ADDRESS).call();
 
-      const nonce = await contract.methods.getNonce(publicAddress).call();
-      
-      //same helper constructMetaTransactionMessage used in SDK front end code
-      const messageToSign = constructMetaTransactionMessage(
-        nonce,
-        config.POLY_NETWORK_ID,
-        functionSignature,
-        config.POLY_SPN_TOKEN_ADDRESS
-      );
-
-      const { signature } = web3.eth.accounts.sign(
-        "0x" + messageToSign.toString("hex"),
-        privateKey
-      );
-      console.log('signature...', signature);
-
-      const { r, s, v } = getSignatureParameters(web3, signature); // same helper used in SDK frontend code
-
-      console.log('r,s,v', r, s, v);
-
-      const executeMetaTransactionData = contract.methods
-        .executeMetaTransaction(publicAddress, functionSignature, r, s, v)
-        .encodeABI();
-
-      console.log('contractSignature', executeMetaTransactionData);
-
-
-      const txParams = {
+      let message = {
         nonce: web3.utils.toHex(nonce),
-        to: config.POLY_SPN_TOKEN_ADDRESS,
-        data: executeMetaTransactionData,
+        from: config.USER_ADDRESS,
+        functionSignature: functionSignature,
       };
+
+      //please refer to SDK front end example for domainType and domainData
+      const dataToSign = {
+        types: {
+          EIP712Domain: domainType,
+          MetaTransaction: metaTransactionType
+        },
+        domain: domainData,
+        primaryType: "MetaTransaction",
+        message: message
+      };
+
+      /*Its important to use eth_signTypedData_v3 and not v4 to get EIP712 
+      signature because we have used salt in domain data instead of chainId*/
+      const signature = signTypedData_v4(Buffer.from(config.PRIVATE_KEY, 'hex'), { data: dataToSign });
+      let { r, s, v } = getSignatureParameters(web3, signature); // same helper used in SDK frontend code
+      const executeMetaTransactionData = contract.methods.executeMetaTransaction(config.USER_ADDRESS, functionSignature, r, s, v).encodeABI();
+
+      // Build the transaction
+      const txObject = {
+        nonce: web3.utils.toHex(nonce),
+        to: config.POLY_BADGE_STORE_ADDRESS,
+        data: executeMetaTransactionData,
+      }
 
       const common = Common.custom(CustomChain.PolygonMumbai);
 
-      // Sign the transaction
-      const tx = Tx.fromTxData(txParams, { common });
-      const signedTx = tx.sign(Buffer.from(privateKey, 'hex'));
+      const tx = Tx.fromTxData(txObject, { common });
+      const signedTx = tx.sign(Buffer.from(config.PRIVATE_KEY, 'hex'));
       const serializedTx = signedTx.serialize();
       const raw = '0x' + serializedTx.toString('hex');
 
-      web3.eth
-        .sendSignedTransaction(raw)
-        .once('transactionHash', (hash) => console.log('transactionHash', hash))
-        .once('receipt', (rec) => console.log('receipt', rec))
-        .once('error', (err) => console.log('error...', err));
+      console.log(raw);
+
+      /* web3.eth.sendSignedTransaction(raw)
+        .once('transactionHash', (hash: string) => console.log(hash))
+        .once('receipt', (rec) => console.log(rec))
+        .once('error', (err) => console.log(err)); */
 
     })
     .onEvent(biconomy.ERROR, (error, message) => {
